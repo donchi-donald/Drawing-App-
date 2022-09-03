fb306be5588a2fc236a342868a4209878dff9a55
donchi-donald <donchidonald2000@gmail.com>
2022-09-03 12:58:59 +0200

    add drawline, colorEvent

--- script.js
+++ script.js
@@ -21,6 +21,22 @@ function drawCircle(x, y){
 
 }
 
+function drawLine(x1, y1, x2, y2){
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size*2;
+    ctx.stroke();
+
+}
+
+colorEl.addEventListener('change', (e)=>{
+    color = e.target.value;
+} )
+
+
+
 canvas.addEventListener('mousedown', (e) =>{
     isPressed = true;
     x= e.offsetX;
@@ -28,12 +44,18 @@ canvas.addEventListener('mousedown', (e) =>{
     //console.log(isPressed, x, y);
 })
 
+
+
 canvas.addEventListener('mousemove', (e) =>{
    if(isPressed){
        const x2 = e.offsetX;
        const y2 = e.offsetY;
 
        drawCircle(x2, y2);
+       drawLine(x, y, x2, y2);
+
+       x = x2;
+       y = y2;
    }
     
 })
